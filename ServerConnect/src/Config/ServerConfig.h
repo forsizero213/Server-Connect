7de35b21f1e881d4323b8da9dc46825d628b0c8e
forsizero <kirilforsizero@gmail.com>
2026-08-17 10:01:05 +0300

    Refactor ServerConfig.h to include server data fetching
    Removed copyright notice and added server data structure and fetch function.

--- ServerConnect/src/Config/ServerConfig.h
+++ ServerConnect/src/Config/ServerConfig.h
@@ -1,31 +1,55 @@
-/*
- * Copyright (c) 2026 CanVas Dev
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+#ifndef SERVER_CONFIG_H
+#ifndef SERVER_CONFIG_H
+#define SERVER_CONFIG_H
 
-#pragma once
+#include <windows.h>
+#include <wininet.h>
+#include <string>
+#include <nlohmann/json.hpp>
 
-#include "Settings.h"
+#pragma comment(lib, "wininet.lib")
 
-namespace CanVasDev::ServerConnect::Config::ServerConfig {
-    struct ServerSettings {
-        std::string_view host = DEFAULT_HOST;
-        int port = DEFAULT_PORT;
+using json = nlohmann::json;
 
-        std::string_view JsonFilename = JSON_FILENAME;
-        bool useJson = USE_JSON_CONFIG;
-    };
+struct ServerData {
+    std::string ip;
+    std::string port;
+};
 
-	inline constexpr ServerSettings DefaultSettings;
+inline ServerData FetchServerConfig() {
+    ServerData data = { "", "" };
+    
+    HINTERNET hInternet = InternetOpenA("ServerConnectClient/1.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    if (!hInternet) return data;
+
+    HINTERNET hConnect = InternetOpenUrlA(hInternet, "http://elitex.fun/ip", NULL, 0, INTERNET_FLAG_RELOAD, 0);
+    if (!hConnect) {
+        InternetCloseHandle(hInternet);
+        return data;
+    }
+
+    std::string response;
+    char buffer[1024];
+    DWORD bytesRead = 0;
+
+    while (InternetReadFile(hConnect, buffer, sizeof(buffer) - 1, &bytesRead) && bytesRead > 0) {
+        buffer[bytesRead] = '\0';
+        response += buffer;
+    }
+
+    InternetCloseHandle(hConnect);
+    InternetCloseHandle(hInternet);
+
+    try {
+        // Парсинг масиву {"192.168.0.1", "7777"}
+        auto parsedJson = json::parse(response);
+        if (parsedJson.is_array() && parsedJson.size() >= 2) {
+            data.ip = parsedJson[0].get<std::string>();
+            data.port = parsedJson[1].get<std::string>();
+        }
+    } catch (...) {}
+
+    return data;
 }
+
+#endif // SERVER_CONFIG_H
