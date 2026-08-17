b8b3d7434c6836186b43d1cd5816c90f3e9404a2
forsizero <kirilforsizero@gmail.com>
2026-08-17 10:12:45 +0300

    Update ServerConfig.h

--- ServerConnect/src/Config/ServerConfig.h
+++ ServerConnect/src/Config/ServerConfig.h
@@ -1,11 +1,10 @@
 #ifndef SERVER_CONFIG_H
-#ifndef SERVER_CONFIG_H
 #define SERVER_CONFIG_H
 
 #include <windows.h>
 #include <wininet.h>
 #include <string>
-#include <json.hpp>
+#include "json.hpp"
 
 #pragma comment(lib, "wininet.lib")
 
@@ -17,37 +16,37 @@ struct ServerData {
 };
 
 inline ServerData FetchServerConfig() {
-    ServerData data = { "", "" };
+    ServerData data;
+    HINTERNET hInternet = InternetOpenA("ServerConnectAgent", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
     
-    HINTERNET hInternet = InternetOpenA("ServerConnectClient/1.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
-    if (!hInternet) return data;
+    if (hInternet) {
+        HINTERNET hConnect = InternetOpenUrlA(hInternet, "https://elitex.fun/ip", NULL, 0, INTERNET_FLAG_RELOAD | INTERNET_FLAG_SECURE, 0);
+        
+        if (hConnect) {
+            char buffer[512];
+            DWORD bytesRead = 0;
+            std::string response = "";
 
-    HINTERNET hConnect = InternetOpenUrlA(hInternet, "http://elitex.fun/ip", NULL, 0, INTERNET_FLAG_RELOAD, 0);
-    if (!hConnect) {
-        InternetCloseHandle(hInternet);
-        return data;
-    }
+            while (InternetReadFile(hConnect, buffer, sizeof(buffer) - 1, &bytesRead) && bytesRead > 0) {
+                buffer[bytesRead] = '\0';
+                response += buffer;
+            }
 
-    std::string response;
-    char buffer[1024];
-    DWORD bytesRead = 0;
+            try {
+                auto parsedJson = json::parse(response);
+                if (parsedJson.is_array() && parsedJson.size() >= 2) {
+                    data.ip = parsedJson[0].get<std::string>();
+                    data.port = parsedJson[1].get<std::string>();
+                }
+            }
+            catch (...) {
+                // У разі помилки парсингу JSON повертаються порожні значення
+            }
 
-    while (InternetReadFile(hConnect, buffer, sizeof(buffer) - 1, &bytesRead) && bytesRead > 0) {
-        buffer[bytesRead] = '\0';
-        response += buffer;
-    }
-
-    InternetCloseHandle(hConnect);
-    InternetCloseHandle(hInternet);
-
-    try {
-        // Парсинг масиву {"192.168.0.1", "7777"}
-        auto parsedJson = json::parse(response);
-        if (parsedJson.is_array() && parsedJson.size() >= 2) {
-            data.ip = parsedJson[0].get<std::string>();
-            data.port = parsedJson[1].get<std::string>();
+            InternetCloseHandle(hConnect);
         }
-    } catch (...) {}
+        InternetCloseHandle(hInternet);
+    }
 
     return data;
 }
